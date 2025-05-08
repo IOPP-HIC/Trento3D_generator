eb546b791adff5cb2df69dac4c53ca088a9ee89e
Xiang-Yu Wu <xiangyu.wu2@mail.mcgill.ca>
2025-05-08 17:03:06 -0400

    update trento 3D

--- get_trento3D.sh
+++ get_trento3D.sh
@@ -2,8 +2,8 @@
 
 
 rm -fr trento3d_subnucleon
-git clone --depth=1 git@github.com:xyw2016/trento3D_subnucleon.git -b master trento3d_subnucleon
-commitHash="dded85bbe1a06eb93abcfbe406cf598226d22894"
+git clone --depth=1 https://github.com/xyw2016/trento3D_subnucleon.git -b master trento3d_subnucleon
+commitHash="d6913d93053fef8729e988b10a1a706d6e17fa8c"
 cd trento3d_subnucleon 
 git checkout $commitHash
 rm -fr .git
